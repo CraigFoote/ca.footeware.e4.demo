118f1849b1452d8b3550cf816914b2c60bac2f3b
Craig Foote <CraigFoote@gmail.com>
2026-09-01 15:35:53 -0400

    fiddling with product build

---
+++ ca.footeware.e4.parent/bundles/ca.footeware.e4.app/src/ca/footeware/e4/app/Application.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package ca.footeware.e4.app;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+/**
+ * 
+ */
+public class Application implements IApplication {
+
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
